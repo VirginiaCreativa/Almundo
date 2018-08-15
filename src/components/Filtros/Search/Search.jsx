27b259f41c8bdd97d6d6c9af9f4e213497039eb9
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-08-15 17:38:28 -0500

    Valida Search heading side

--- src/components/Filtros/Search/Search.jsx
+++ src/components/Filtros/Search/Search.jsx
@@ -3,21 +3,26 @@ import { Container, Row, Col } from "reactstrap";
 import classes from "./Search.scss";
 import Aux from "./../../../Hoc/Aux.jsx";
 
-const search = props => (
-  <Aux>
-    <div className={classes.SpaceBox}>
-      <Row>
-        <Col md="10">
-          <div className={classes.Heading}>
-            <i className={classes.iconSearch} />
-            <p> Nombre del hotel</p>
-          </div>
-        </Col>
-        <Col md="10">
-          <i className={classes.IconHide} />
-        </Col>
-      </Row>
-    </div>
-  </Aux>
-);
+const search = props => {
+  return (
+    <Aux>
+      <div className={classes.SpaceBox}>
+        <div className={classes.Search}>
+          <Row>
+            <Col md="9">
+              <div className={classes.Heading}>
+                <i className={classes.iconSearch} />
+                <p> Nombre del hotel</p>
+              </div>
+            </Col>
+            <Col md="3">
+              <i className={classes.iconTriangleDown} onClick={props.btnSide} />
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </Aux>
+  );
+};
+
 export default search;
