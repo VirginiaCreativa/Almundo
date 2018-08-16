e62cd3cb8fc8d2d04d4a2b80aee32063acece34e
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-08-15 21:31:40 -0500

    Valida deracion stars

--- src/components/Filtros/Stars/Stars.jsx
+++ src/components/Filtros/Stars/Stars.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from "reactstrap";
 import classes from "./Stars.scss";
 import HeadingFiltros from "./../../UI/HeadingFiltros.jsx";
 
@@ -13,6 +22,89 @@ const stars = props => {
           icon="iconStar"
           typeslider="iconTriangleUp"
         />
+        <Form>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                checked={props.checked}
+                onChange={props.onChanged}
+                value={props.valuered}
+                name="todas"
+              />
+              Todas las estrellas
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                checked={props.checked}
+                value={props.valuered}
+                name="5"
+              />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                checked={props.checked}
+                onChange={props.onChanged}
+                value={props.valuered}
+                name="4"
+              />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                checked={props.checked}
+                onChange={props.onChanged}
+                value={props.valuered}
+                name="3"
+              />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                checked={props.checked}
+                onChange={props.onChanged}
+                value={props.valuered}
+                name="2"
+              />
+              <i className={classes.iconCheck} />
+              <i className={classes.iconCheck} />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                checked={props.checked}
+                onChange={props.onChanged}
+                value={props.valuered}
+                name="1"
+              />
+              <i className={classes.iconCheck} />
+            </Label>
+          </FormGroup>
+        </Form>
       </Container>
     </div>
   );
