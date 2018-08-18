bb0780421d763479800074ea1582103298613046
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-08-17 22:17:22 -0500

    Slide Animation Filtar

--- src/components/FiltrosMobile/FiltrosMobile.jsx
+++ src/components/FiltrosMobile/FiltrosMobile.jsx
@@ -3,8 +3,11 @@ import { Container, Row, Col } from "reactstrap";
 import SliderMobile from "./SliderMobile.jsx";
 import classes from "./FiltrosMobile.scss";
 import Aux from "./../../Hoc/Aux.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 
 const filtrosMobile = props => {
+  const { btnSlided, iconTriangle, slidered } = props;
   const mClass = [
     classes.FiltrosMobile,
     "d-block",
@@ -13,24 +16,37 @@ const filtrosMobile = props => {
     "d-lg-none"
   ].join(" ");
 
+  let heightShow = {
+    minHeight: "0px",
+    height: "0px"
+  };
   let filtrar = null;
-  if (props.slidered) {
-    filtrar = <SliderMobile />;
-  }
-  let icon = classes.iconTriangleUp;
-  if (props.iconTriangle) {
-    icon = classes.iconTriangleDown;
+  if (slidered) {
+    filtrar = <SliderMobile sliderShow={slidered} />;
+    heightShow.minHeight = "236px";
   }
   return (
     <Aux>
       <div className={mClass}>
         <Container>
-          <div className={classes.MenuFiltros}>
-            <h5>Filtrar</h5>
-            <i className={icon} onClick={props.btnSlided} />
-          </div>
-          <div className={classes.SliderFiltar}>{filtrar}</div>
+          <a href="#" className={classes.MenuFiltros} onClick={btnSlided}>
+            Filtrar
+            <FontAwesomeIcon
+              icon={faCaretUp}
+              style={{
+                transform: iconTriangle ? "rotate(0deg)" : "rotate(180deg)",
+                transition: "all .25s ease",
+                position: "relative",
+                left: "10px",
+                top: "2px"
+              }}
+            />
+          </a>
         </Container>
+      </div>
+
+      <div className={classes.SliderFiltar} style={{ ...heightShow }}>
+        <Container>{filtrar}</Container>
       </div>
     </Aux>
   );
